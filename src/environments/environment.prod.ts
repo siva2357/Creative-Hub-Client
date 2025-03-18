48d672053a8ba5fd88ab44d33c9faf03badc7fc5
Siva Prasad Kurra <sivakurra.ksp2357@gmail.com>
2025-03-18 22:15:16 +0530

    checking deployment status agina

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
 export const environment = {
   production: true,
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: ''
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  }
 };
