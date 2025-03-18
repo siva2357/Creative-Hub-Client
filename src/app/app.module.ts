78695ed520c28d523116de7364293c36e80e164a
Siva Prasad Kurra <sivakurra.ksp2357@gmail.com>
2025-03-18 18:35:37 +0530

    sample testing for firebase storage file upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig), // Use legacy compatibility mode
+
     AngularFireStorageModule, // Use storage services
   ],
   providers: [],
