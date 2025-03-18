e961fb7ece34d9d7c845b6be2bfaf07eb7ca79a3
Siva Prasad Kurra <sivakurra.ksp2357@gmail.com>
2025-03-18 22:05:35 +0530

    checking logic again for ennvironment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
+};
